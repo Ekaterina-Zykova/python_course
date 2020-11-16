bde2dfeae98337a93f4b0e79e3cc7d91b51c85f9
enzkva <enzkva@gmail.com>
2020-11-16 09:49:01 +0300

    added instruction how to run doctests

--- homework4/task04/doctest.py
+++ homework4/task04/doctest.py
@@ -25,7 +25,7 @@ You will learn:
  - how to write instructions
 
 
->>> fizzbuzz(5)
+>> fizzbuzz(5)
 ["1", "2", "fizz", "4", "buzz"]
 
 * https://en.wikipedia.org/wiki/Fizz_buzz
@@ -35,4 +35,31 @@ from typing import List
 
 
 def fizzbuzz(n: int) -> List[str]:
-    pass
+    """
+    How to run doctests:
+     - Install Python 3.9 (https://www.python.org/downloads/)
+     - Install pytest `pip install pytest`
+     - Clone the repository https://github.com/enzkva/python_course
+     - Checkout branch hw04
+     - Open terminal
+     - Run pytest --doctest-modules homework4/task04/doctest.py
+     - For more informstion, run pytest --doctest-modules homework4/task04/doctest.py -v
+
+    Return n FizzBuzz numbers
+    >>> fizzbuzz(5)
+    ['1', '2', 'fizz', '4', 'buzz']
+    >>> fizzbuzz(15)
+    ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz']
+    """
+    fizzbuzz_list = []
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                fizzbuzz_list.append("fizzbuzz")
+            else:
+                fizzbuzz_list.append("fizz")
+        elif i % 5 == 0:
+            fizzbuzz_list.append("buzz")
+        else:
+            fizzbuzz_list.append(str(i))
+    return fizzbuzz_list
