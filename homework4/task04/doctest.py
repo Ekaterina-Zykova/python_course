7a8bd9e0bf8bd9b13c76a529df0dd057d1b81879
enzkva <enzkva@gmail.com>
2020-11-19 19:59:29 +0300

    fix instruction

--- homework4/task04/doctest.py
+++ homework4/task04/doctest.py
@@ -36,15 +36,6 @@ from typing import List
 
 def fizzbuzz(n: int) -> List[str]:
     """
-    How to run doctests:
-     - Install Python 3.9 (https://www.python.org/downloads/)
-     - Install pytest `pip install pytest`
-     - Clone the repository https://github.com/enzkva/python_course
-     - Checkout branch hw04
-     - Open terminal
-     - Run pytest --doctest-modules homework4/task04/doctest.py
-     - For more informstion, run pytest --doctest-modules homework4/task04/doctest.py -v
-
     Return n FizzBuzz numbers
     >>> fizzbuzz(5)
     ['1', '2', 'fizz', '4', 'buzz']
